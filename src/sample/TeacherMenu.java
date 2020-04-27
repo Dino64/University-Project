d2f2d048ebb99bc67129c405505b65cf486ef1e4
Jussi <martin.jussi83@hotmail.se>
2020-04-27 11:36:33 +0200

    small update

---
+++ src/sample/TeacherMenu.java
@@ -0,0 +1,19 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TeacherMenu implements Initializable {
+
+    @FXML Button studentMenu;
+    @FXML Button roomMenu;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
