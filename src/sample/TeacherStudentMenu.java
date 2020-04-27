cdf878679d4ab91c713313bb7264cdedecea9f01
Jussi <martin.jussi83@hotmail.se>
2020-04-27 12:18:27 +0200

    update

--- src/sample/TeacherStudentMenu.java
+++ src/sample/TeacherStudentMenu.java
@@ -8,6 +8,6 @@ import java.util.ResourceBundle;
 public class TeacherStudentMenu implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        
+
     }
 }
