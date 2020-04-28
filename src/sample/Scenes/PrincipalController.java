887a3f1161a59060bba0c047f887699f578d7882
Dino64 <dino.dsu@gmail.com>
2020-04-28 11:32:21 +0200

    mycket problem men vi fixar sdk

--- src/sample/Scenes/PrincipalController.java
+++
@@ -1,5 +0,0 @@
-package sample.Scenes;
-
-public class PrincipalController {
-
-}
---
+++ src/sample/Scenes/PrincipalMenu/PrincipalController.java
@@ -0,0 +1,21 @@
+package sample.Scenes.PrincipalMenu;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PrincipalController implements Initializable {
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    @FXML
+    private void PrincipalRoomMenuButton (){
+
+    }
+}
