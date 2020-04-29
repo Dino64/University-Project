ea8b94b6a43277840611b5128ab62fa84fb949f5
afgsuna <your emailafgsuna@gmail.comgit config --global user.email your>
2020-04-29 18:18:47 +0200

    Signup scene designed

--- src/sample/Scenes/SignUpScene/SignUpScene.java
+++ src/sample/Scenes/SignUpScene/SignUpScene.java
@@ -1,4 +1,23 @@
-package sample.Scenes.SignUpScene;
+package src.sample.Scenes.SignUpScene;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 public class SignUpScene {
+
+    @FXML private TextField
+            txtFldFirstName,
+            txtFldLastName,
+            txtFldSsn,
+            txtFldStreet,
+            txtFldZip,
+            txtFldEmail,
+            txtFldPassword,
+            txtFldConfirmPassword,
+            txtFldPhone,
+            txtFldState,
+            txtFldPhoneHome;
+
+
+
 }
