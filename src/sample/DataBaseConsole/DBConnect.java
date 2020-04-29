84bbecf6aef4663bc756727e160497cb313f46aa
Dino64 <dino.dsu@gmail.com>
2020-04-29 16:50:36 +0200

    database slight impovement

--- src/sample/DataBaseConsole/DBConnect.java
+++ src/sample/DataBaseConsole/DBConnect.java
@@ -4,10 +4,8 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 public class DBConnect {
     public static void main(String[] args) throws SQLException {
-        String url = "\"jdbc:mysql://localhost:3306/UniDatabase2";
-        String username = "root";
-        String pass = "root";
+        String url ="jdbc:mysql://127.0.0.1:localhost:3306/UniDatabase2?user=root&password=root&serverTimezone=UTC&useSSL=false";
 
-        Connection con = DriverManager.getConnection(url,username,pass);
+        Connection con = DriverManager.getConnection(url);
     }
 }
