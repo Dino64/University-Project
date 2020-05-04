e1575f97c6d6843dc44593eceaf606061b20a423
Dino64 <dino.dsu@gmail.com>
2020-05-04 12:14:29 +0200

    Minor changes

--- src/sample/Scenes/LoginScene/LoginScene.java
+++ src/sample/Scenes/LoginScene/LoginScene.java
@@ -12,6 +12,7 @@ import javafx.scene.control.TextField;
 import sample.Model.Principal;
 import sample.Model.Student;
 import sample.Model.Teacher;
+import sample.Scenes.Teacher.TeacherStudentMenu;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -19,10 +20,13 @@ import java.util.ResourceBundle;
 
 
 public class LoginScene implements Initializable {
+LoginScene login = new LoginScene();
+TeacherStudentMenu stuMenu = new TeacherStudentMenu();
+
     String userName;
     String password;
     Student studentUlf = new Student("Ulf","Ulfson","231674539021","Ulf.Ulffson@myspace.net"
-    ,"Ulf_Likes_BIGwomen","555 567 389","1");
+    ,"Ulf_Likes_BIGwomen","555 567 389","1","Biologi");
 
     Principal principalMagnus = new Principal("Magnus","Rot","435908671243","Magnus.jehova@yahoo.RU",
             "SloopyJoe","555 673 196","Head office");
@@ -41,6 +45,7 @@ public class LoginScene implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
 
     }
     }
--- src/sample/Scenes/Teacher/TeacherStudentMenu.java
+++ src/sample/Scenes/Teacher/TeacherStudentMenu.java
@@ -8,24 +8,39 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import sample.Model.Student;
+import sample.Model.Teacher;
+import sample.Model.User;
+import sample.Scenes.LoginScene.LoginScene;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class TeacherStudentMenu implements Initializable {
+    LoginScene login = new LoginScene();
+    TeacherStudentMenu stuMenu = new TeacherStudentMenu();
     @FXML
     Button backTeach, addGrade;
     @FXML
     TextField searchStudent , setGrade;
+    @FXML
+    TableView StudentTableView;
     @FXML
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
     }
+
+    @FXML
+    private void LoadStudentButton(){
+
+    }
+
     @FXML
     private void backButton(ActionEvent be){
         Node node = (Node) be.getSource();
