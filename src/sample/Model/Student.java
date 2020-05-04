2e3e46146ae0a5520c3ec0c3b2614923d5d19403
Dino64 <dino.dsu@gmail.com>
2020-05-04 11:27:28 +0200

    Added registeredcourse to student

--- src/sample/Model/Student.java
+++ src/sample/Model/Student.java
@@ -4,10 +4,16 @@ package sample.Model;
 public class Student extends User {
 
     private String studentID;
+    private String registeredCourse;
 
-    public Student(String firstName, String lastName, String ssn, String email1, String password, String phoneNumber, String studentID) {
+    public String getRegisteredCourse() {
+        return registeredCourse;
+    }
+
+    public Student(String firstName, String lastName, String ssn, String email1, String password, String phoneNumber, String studentID, String registeredCourse) {
         super(firstName, lastName, ssn, email1, password, phoneNumber);
         this.studentID = studentID;
+        this.registeredCourse = registeredCourse;
     }
 
     public String getStudentID() {
