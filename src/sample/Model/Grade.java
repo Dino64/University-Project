e14c23e25d1bc8db4f677a7e7f861ca1237f5b0c
Dino64 <dino.dsu@gmail.com>
2020-05-05 14:50:20 +0200

    added grade class

---
+++ src/sample/Model/Grade.java
@@ -0,0 +1,13 @@
+package sample.Model;
+
+public class Grade {
+    double grade;
+
+    public double getGrade() {
+        return grade;
+    }
+
+    public void setGrade(double grade) {
+        this.grade = grade;
+    }
+}
