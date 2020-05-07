c2b4d0ba576e1bad2cef19182d5d8676ed97ba46
jenslindstrom <jl_3570@hotmail.com>
2020-05-07 20:38:33 +0200

    update backbutton

--- src/sample/Scenes/Student/StudentRegisterNewCourse.java
+++ src/sample/Scenes/Student/StudentRegisterNewCourse.java
@@ -1,16 +1,31 @@
 package sample.Scenes.Student;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class StudentRegisterNewCourse implements Initializable {
-        @Override
-        public void initialize(URL url, ResourceBundle resourceBundle) {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
 
     }
-   
 
+    public void backButton(ActionEvent event) throws IOException {
+        Node node = (Node) event.getSource();
+        Stage stage = (Stage) node.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("StudentMyPage.fxml"));
+        Scene scene = new Scene(root);
+        stage.setTitle("Student menu");
+        stage.setScene(scene);
+
+    }
 }
 
