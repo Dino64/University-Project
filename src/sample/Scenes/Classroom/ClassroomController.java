9a0ba6cbed9fce5743a92e1f04e61f054c7d7e7b
Dino64 <dino.dsu@gmail.com>
2020-05-14 10:13:21 +0200

    commit

--- src/sample/Scenes/Classroom/ClassroomController.java
+++ src/sample/Scenes/Classroom/ClassroomController.java
@@ -1,4 +1,28 @@
 package sample.Scenes.Classroom;
 
-public class ClassroomController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import sample.DataBaseConsole.DBConnect;
+import sample.Model.Classroom;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ClassroomController implements Initializable {
+    Classroom hej = new Classroom();
+
+    DBConnect db = new DBConnect();
+    @FXML
+    Label labelLabel;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    @FXML
+    public void addButton(){
+        db.addClassRoom();
+    }
 }
