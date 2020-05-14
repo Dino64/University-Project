5791223da9b80d7324c88548ea74f8f49e6c0b41
afgsuna <your emailafgsuna@gmail.comgit config --global user.email your>
2020-05-14 15:25:58 +0200

    Methods

--- src/sample/Scenes/Classroom/ClassroomController.java
+++ src/sample/Scenes/Classroom/ClassroomController.java
@@ -2,7 +2,9 @@ package sample.Scenes.Classroom;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import sample.DataBaseConsole.DBConnect;
 import sample.Model.Classroom;
@@ -16,13 +18,19 @@ public class ClassroomController implements Initializable {
     DBConnect db = new DBConnect();
 
     @FXML
+    Button addRoomButton,
+    removeRoomButton, editRoomButton, BackButton;
+
+    @FXML
+    TextArea listOfRooms;
+
+    @FXML
     Label labelLabel;
 
     @FXML
-    TextField roomNr,
-            numberOfSeats,
-            courseField,
-            buildingText;
+    TextField numberOfSeats,
+            roomNumber;
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -30,14 +38,25 @@ public class ClassroomController implements Initializable {
     }
 
     @FXML
-    public void addButton(){
+    public void addRoomButton(){
        db.getInstance().connect();
         try {
-            DBConnect.setRoom(new Classroom(roomNr.getText(),numberOfSeats.getText(),false));
+            DBConnect.setRoom(new Classroom(roomNumber.getText(),numberOfSeats.getText(),false));
             DBConnect.getInstance().addClassRoom();
-
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    public void removeRoomButton(){
+
+    }
+
+    public void editRoomButton(){
+
+    }
+
+    public void BackButton(){
+
     }
 }
