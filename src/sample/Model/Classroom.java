6bab229dd226126cda70c640505b5413c65a4099
afgsuna <your emailafgsuna@gmail.comgit config --global user.email your>
2020-05-14 08:57:20 +0200

    Changes

--- src/sample/Model/Classroom.java
+++ src/sample/Model/Classroom.java
@@ -1,11 +1,29 @@
 package sample.Model;
 
-import sample.Model.*;
 
 public class Classroom {
 
-    private String isForSeminar;
-    private String isForGroupWork;
-    private String isForTeaching;
+    private int NumberOfSeats;
+    private int RoomNumber;
 
+    public Classroom(int numberOfSeats, int roomNumber) {
+        NumberOfSeats = numberOfSeats;
+        RoomNumber = roomNumber;
+    }
+
+    public int getNumberOfSeats() {
+        return NumberOfSeats;
+    }
+
+    public void setNumberOfSeats(int numberOfSeats) {
+        NumberOfSeats = numberOfSeats;
+    }
+
+    public int getRoomNumber() {
+        return RoomNumber;
+    }
+
+    public void setRoomNumber(int roomNumber) {
+        RoomNumber = roomNumber;
+    }
 }
