ae3d0d7aa583ef8932c2b8e02bb712cf10e49d00
Dino64 <dino.dsu@gmail.com>
2020-05-14 11:04:57 +0200

    gd

--- src/sample/Scenes/Classroom/ClassroomController.java
+++ src/sample/Scenes/Classroom/ClassroomController.java
@@ -8,10 +8,11 @@ import sample.Model.Classroom;
 
 import java.net.URL;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class ClassroomController implements Initializable {
-
+    private ArrayList<Classroom> listOfRooms = new ArrayList<>();
 
     DBConnect db = new DBConnect();
     @FXML
@@ -25,6 +26,9 @@ public class ClassroomController implements Initializable {
     @FXML
     public void addButton(){
         try {
+            for (int i = 0; i < listOfRooms.size(); i++){
+                labelLabel.setText(String.valueOf(i));
+            }
             DBConnect.getInstance().addClassRoom();
         } catch (SQLException e) {
             e.printStackTrace();
