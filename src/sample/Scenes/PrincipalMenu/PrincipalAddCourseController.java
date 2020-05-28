4fb9997b7cfc725c783d69a87eed292c197cf439
Dino64 <dino.dsu@gmail.com>
2020-05-28 16:55:37 +0200

    added some error handling

--- src/sample/Scenes/PrincipalMenu/PrincipalAddCourseController.java
+++ src/sample/Scenes/PrincipalMenu/PrincipalAddCourseController.java
@@ -2,6 +2,7 @@ package sample.Scenes.PrincipalMenu;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import sample.DataBaseConsole.DBConnect;
@@ -27,15 +28,38 @@ public class PrincipalAddCourseController implements Initializable {
     public void addButton() throws SQLException {
         String course,subject,grade;
         int userID;
-
         course = courseTextField.getText();
         subject = subjectTextField.getText();
         grade = gradeTextField.getText();
-
         userID = Integer.parseInt(idTextField.getText());
+        if (courseTextField.getText().isEmpty()){
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Warning");
+            alert.setHeaderText("Not good");
+            alert.setContentText("Please enter a course");
+            alert.showAndWait();
+        }else if (subjectTextField.getText().isEmpty()){
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Warning");
+            alert.setHeaderText("Not good");
+            alert.setContentText("Please enter a subject");
+            alert.showAndWait();
+        }else if (gradeTextField.getText().isEmpty()){
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Warning");
+            alert.setHeaderText("Not good");
+            alert.setContentText("Please enter a grade");
+            alert.showAndWait();
+        }else if (idTextField.getText().isEmpty()) {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Warning");
+            alert.setHeaderText("Not good");
+            alert.setContentText("Please enter an id");
+            alert.showAndWait();
+        }else {
+            DBConnect.getInstance().addCourse(course,grade,subject,userID);
+        }
 
-
-        DBConnect.getInstance().addCourse(course,grade,subject,userID);
     }
 
     @FXML
