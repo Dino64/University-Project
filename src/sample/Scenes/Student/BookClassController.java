ff059f95b3eb81d358d26dc87e17138a9a29e170
afgsuna <your emailafgsuna@gmail.comgit config --global user.email your>
2020-05-29 19:30:14 +0200

    Show room Table fixed

--- src/sample/Scenes/Student/BookClassController.java
+++ src/sample/Scenes/Student/BookClassController.java
@@ -1,9 +1,13 @@
 package sample.Scenes.Student;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.util.StringConverter;
 import sample.DataBaseConsole.DBConnect;
 import sample.Model.Classroom;
 import sample.Model.SceneChanger;
@@ -24,36 +28,22 @@ public class BookClassController implements Initializable {
 
     @FXML
     Button backButton, bookButton, unBookButton, showClassRoom;
-
-    @FXML
-    TextArea ShowClassTextArea;
-
     @FXML
     TextField bookTextField, unBookTextField;
-
     @FXML
     TableView <Classroom> ShowTable;
-
     @FXML
     TableColumn<Classroom, Integer> roomNumber;
     @FXML
-    TableColumn<Classroom,Integer> numberOfSeats;
+    TableColumn<Classroom, Integer> numberOfSeats;
     @FXML
     TableColumn<Classroom, Boolean> isBooked;
     @FXML
-    TableColumn<Classroom, Date> dateBook;
+    TableColumn<Classroom, Date> Date;
 
-
-
-
-
-    private ArrayList<Classroom> classroom;
 
     @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-
-
-    }
+    public void initialize(URL url, ResourceBundle resourceBundle) {DBConnect.getInstance().connect();}
 
     @FXML
     public void pressBackButton(ActionEvent event) throws IOException {
@@ -86,10 +76,35 @@ public class BookClassController implements Initializable {
 
     }
 
-    @FXML
-    public void pressShowClassroom() throws SQLException {
-        DBConnect.getInstance().connect();
-        ShowClassTextArea.setText(String.valueOf(DBConnect.getInstance().ReadClassroom()));
+//    @FXML
+//    public void pressShowClassroom() throws SQLException {
+//        DBConnect.getInstance().connect();
+//        ShowClassTextArea.setText(String.valueOf(DBConnect.getInstance().ReadClassroom()));
+//    }
+
+    @FXML private void pressShowClassroom(){
+        ShowTable.getItems();
+        ObservableList<Classroom> roomList = DBConnect.getInstance().getRooms();
+        roomNumber.setCellValueFactory(new PropertyValueFactory("RoomNumber"));
+        numberOfSeats.setCellValueFactory(new PropertyValueFactory("NumberOfSeats"));
+        isBooked.setCellValueFactory(new PropertyValueFactory("IsBooked"));
+        Date.setCellValueFactory(new PropertyValueFactory("Date"));
+        ShowTable.setEditable(true);
+        Date.setCellFactory(TextFieldTableCell.forTableColumn(new StringConverter<Date>() {
+            @Override
+            public String toString(Date date) {
+                return null;
+            }
+
+            @Override
+            public Date fromString(String s) {
+                return null;
+            }
+        }));
+        ShowTable.setItems(roomList);
+        System.out.println(roomList.toString());
+        System.out.println("Debug: correct working yeyeyeyeye");
+        //      roomArea.setText(String.valueOf(DBConnect.getInstance().ReadClassroom()));
     }
 
 }
